856b8b85adc2bb2781e2768399cf35843d9afe84
Michael Bock <me@thought2.de>
2020-02-08 10:54:04 +0100

    Fix stage network

--- src/nix/network-stage.nix
+++ src/nix/network-stage.nix
@@ -1,10 +1,6 @@
 {
   webserver = {
-    # deployment.targetHost = "stage.thought2.de";
-    deployment.targetEnv = "virtualbox";
-    deployment.virtualbox.memorySize = 1024; # megabytes
-    deployment.virtualbox.vcpu = 2; # number of cpus
-
+    deployment.targetHost = "stage.thought2.de";
     imports = [ ./hardware-configuration.nix ];
     boot.loader.grub.device = "/dev/sda";
   };
