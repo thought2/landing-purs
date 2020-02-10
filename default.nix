04f06d9cc4b0c04c015fd734ae4e1e734573a168
Michael Bock <me@thought2.de>
2020-02-10 13:39:10 +0100

    Fix travis

--- default.nix
+++ default.nix
@@ -7,15 +7,41 @@ let
 
   yarn2nix' = import sources.yarn2nix { pkgs = nixpkgs; };
 
-in { mkDerivation ? nixpkgs.stdenv.mkDerivation, nixfmt ? nixpkgs.nixfmt
-, spago ? easy-purescript-nix.spago
-, writeShellScriptBin ? nixpkgs.writeShellScriptBin
-, purs ? easy-purescript-nix.purs-0_13_4, yarn2nix ? yarn2nix'
-, yarn ? nixpkgs.yarn, runCommand ? nixpkgs.runCommand
-, spago2nix ? easy-purescript-nix.spago2nix, fetchgit ? nixpkgs.fetchgit
-, make ? nixpkgs.gnumake, bash ? nixpkgs.bash
-, nix-gitignore ? nixpkgs.nix-gitignore, dhall ? nixpkgs.dhall
-, git ? nixpkgs.git, nodejs ? nixpkgs.nodejs, }:
+in {
+
+mkDerivation ? nixpkgs.stdenv.mkDerivation,
+
+nixfmt ? nixpkgs.nixfmt,
+
+spago ? easy-purescript-nix.spago,
+
+writeShellScriptBin ? nixpkgs.writeShellScriptBin,
+
+purs ? easy-purescript-nix.purs-0_13_4,
+
+yarn2nix ? yarn2nix',
+
+yarn ? nixpkgs.yarn,
+
+runCommand ? nixpkgs.runCommand,
+
+spago2nix ? easy-purescript-nix.spago2nix,
+
+fetchgit ? nixpkgs.fetchgit,
+
+make ? nixpkgs.gnumake,
+
+bash ? nixpkgs.bash,
+
+nix-gitignore ? nixpkgs.nix-gitignore,
+
+dhall ? nixpkgs.dhall,
+
+git ? nixpkgs.git,
+
+nodejs ? nixpkgs.nodejs
+
+}:
 let
   packageJsonMeta = {
     pname = "landing-purs";
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-TARGET=$1
-
-nix-env -i nixops
-nixops delete --all --force
-nixops create src/nix/network.nix src/nix/network-$TARGET.nix -d $TARGET
-nixops deploy -d $TARGET
